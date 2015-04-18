75814ff25974fd7b71efe4ec0f042681c50d0274
ilsa23 <ilze.sarai@gmail.com>
2015-04-18 14:08:54 -0500

    asd

--- js/contactos.js
+++ js/contactos.js
@@ -8,57 +8,46 @@ var contactos = {
         name.givenName = nombre;
         name.familyName = 'Practica';
         
-        var phones = new [];
+        var phones = [];
         phones[0] = new ContactField('home',tel,true);
-        phones[1]= new ContactField('work','145134',false);
+        phones[1] = new ContactField('work','1234567890',false);
         
-        var emai = new [];
-        emai[0]= new ContactField('home',mail,true);
-        emai[1]= new ContactField('work','adfvafg@gmail.com',false);
-        
-        
+        var mails = [];
+        mails[0] = new ContactField('personal',mail,false);
+        mails[1] = new ContactField('work','prue@prue.com',true);
         
         var contacto = {
             'displayName': nombre,
             'nickname': nombre,
             'name': name,
             'phoneNumbers': phones,
-            'emails': emai
+            'emails': mails
         };
-        
-        var miContacto =        navigator.contacts.create(contacto);
+        var miContacto = navigator.contacts.create(contacto);
         miContacto.save(contactos.guardado,contactos.error);
-        
     },
-    
     guardado: function(){
         navigator.notification.alert('Contacto guardado satisfactoriamente',null,'Contactos','Gracias');
     },
-    
     error: function(err){
-        navigator.notification.alert('Error: '+ err.code,null,'Contactos','Aceptar');
+        navigator.notification.alert('Error: '+err.code,null,'Contactos','Aceptar');
     },
-    
     listar: function(){
-        var filtro= ['*'];
+        var filtro = ['*'];
         var opc = new ContactFindOptions();
-        opc.filter='';
-                navigator.contacts.find(filtro,contactos.lista,contactos.error,opc);
+        opc.filter = '';
+        navigator.contacts.find(filtro,contactos.lista,contactos.error,opc);
     },
-    
-    lista:function(c){
+    lista: function(c){
         $('#cMostrar').html('');
         var cont = 0;
-        for(i=0;i<c.length;i++)
-        {
+        for(i=0;i<c.length;i++){
             var nom = c[i].name.formatted;
             var tel = c[i].tel[0].value;
-            cont ++;
-            $('#cMostrar').append('<li><a href="Tel: '+tel+'">'+nom+'</a></li>');      
+            cont++;
+            $('#cMostrar').append('<li><a href="tel:'+tel+'">'+nom+'</a></li>');
         }
         if(cont==0)
-            $('#cMostrar').html('<li>Sin leer contactos</li>');
+            $('#cMostrar').html('<li>Sin Leer Contactos</li>');
     }
-    
-     
 };
